40375b2ea61eb02beeebf6fe4de15d6090df5a37
sam <smorrison@onlineimage.com>
2019-02-27 16:49:24 -0700

    Added validation to the contact form.

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import router from './router.js'
 import NavBar from './components/NavBar.vue'
 import FooterBar from './components/Footer.vue'
+import VueMaterial from 'vue-material'
+//import VeeValidate from 'vee-validate'
 import './assets/bootstrap-4.0.0-dist/css/bootstrap.min.css'
 import './assets/fontawesome-free-5.7.2-web/css/all.min.css'
-import './styles/main.css'
 import 'vue-material/dist/vue-material.min.css'
-import './assets/images/Me.JPG'
-import VueMaterial from 'vue-material'
 import 'vue-material/dist/theme/default.css'
+import './styles/main.css'
+import './assets/images/Me.JPG'
+
 
 window.Vue = Vue;
 Vue.use(VueMaterial);
+//Vue.use(VeeValidate);
 const app = new Vue({
     el: "#app",
     router,
--- src/components/SimpleForm.vue
+++ src/components/SimpleForm.vue
@@ -1,27 +1,35 @@
 <template>
     <div>
-        <form class="md-layout">
+        <form novalidate class="md-layout" @submit.prevent="validateForm">
             <md-card class="md-layout-item bg-light">
                 <md-card-content>
-                    <md-field>
+                    <md-field :class="getValidationClass('firstName')">
                         <label>First Name</label>
                         <md-input v-model="firstName" required></md-input>
+                        <span class="md-error" v-if="!$v.firstName.required">The first name is required</span>
+                        <span class="md-error" v-else-if="!$v.firstName.minlength">Invalid first name</span>
                     </md-field>
 
-                    <md-field>
+                    <md-field :class="getValidationClass('lastName')">
                         <label>Last Name</label>
                         <md-input v-model="lastName" required></md-input>
+                        <span class="md-error" v-if="!$v.lastName.required">The last name is required</span>
+                        <span class="md-error" v-else-if="!$v.lastName.minlength">Invalid last name</span>
                     </md-field>
-                    <md-field>
+                    <md-field :class="getValidationClass('email')">
                         <label>Email</label>
-                        <md-input v-model="email" required></md-input>
+                        <md-input type="email" v-model="email" required></md-input>
+                        <span class="md-error" v-if="!$v.email.required">The email is required</span>
+                        <span class="md-error" v-else-if="!$v.email.email">Invalid email</span>
                     </md-field>
-                    <md-field>
+                    <md-field :class="getValidationClass('message')">
                         <label>Message</label>
                         <md-textarea v-model="message" required></md-textarea>
+                        <span class="md-error" v-if="!$v.message.required">The first name is required</span>
+                        <span class="md-error" v-else-if="!$v.message.minlength">Invalid first name</span>
                     </md-field>
                     <md-card-actions>
-                        <md-button class="md-dense md-raised" :class="sendEnabled" @click="send" @keyup.enter="send" :disabled="disabled">Send</md-button>
+                        <md-button class="md-dense md-raised bg-primary text-light" type="submit">Send</md-button>
                     </md-card-actions>
                 </md-card-content>
                 <md-progress-bar md-mode="indeterminate" v-if="sending"/>
@@ -32,8 +40,16 @@
 </template>
 
 <script>
+    import { validationMixin } from 'vuelidate'
+    import {
+        required,
+        email,
+        minLength,
+        maxLength
+    } from 'vuelidate/lib/validators'
     export default {
         name: "SimpleForm",
+        mixins: [validationMixin],
         data() {
             return {
                 firstName: null,
@@ -42,10 +58,37 @@
                 message: null,
                 messageSent: false,
                 sending: false,
-                formDisabled: false
+                formDisabled: false,
+                hasErrors: false
+            }
+        },
+        validations: {
+            firstName: {
+                required,
+                minLength: minLength(3)
+            },
+            lastName: {
+                required,
+                minLength: minLength(3)
+            },
+            email: {
+                required,
+                email
+            },
+            message: {
+                required,
+                minLength: minLength(1)
             }
         },
         methods: {
+            getValidationClass(fieldName) {
+                const field = this.$v[fieldName];
+                if(field) {
+                    return {
+                        'md-invalid': field.$invalid && field.$dirty
+                    }
+                }
+            },
             send() {
                 this.sending = true;
                 this.messageSent = true;
@@ -54,24 +97,18 @@
                 }, 1500);
             },
             resetForm() {
+                this.$v.$reset();
                 this.firstName = null;
                 this.lastName = null;
                 this.email = null;
                 this.message = null;
                 this.messageSent = false;
                 this.sending = false;
-            }
-        },
-        computed: {
-            disabled() {
-                return this.formDisabled = (this.firstName === null || this.firstName === "" || this.lastName === null || this.lastName === "" || this.email === null || this.email === "" || this.message === null || this.message === "");
             },
-            sendEnabled() {
-                if(!this.formDisabled) {
-                    return {
-                        'bg-primary': true,
-                        'text-light': true
-                    }
+            validateForm () {
+                this.$v.$touch();
+                if(!this.$v.$invalid) {
+                    this.send();
                 }
             }
         }
@@ -79,7 +116,10 @@
 </script>
 
 <style lang="scss" scoped>
-    small {
-        display: block;
+    .md-progress-bar {
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
     }
 </style>
